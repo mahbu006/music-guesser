bb81cf329d25ad57c01afeb85303fbcb5ef8a1e8
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-04-02 01:36:47 -0500

    added credentials

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 var SpotifyWebApi = require('spotify-web-api-node');
-var spotifyApi = new SpotifyWebApi();
+var spotifyApi = new SpotifyWebApi({
+    clientId: '3330af03c9b249c78e72aed84b9720f1',
+    clientSecret: '7a7fc20450c240bcb9d702858ae639ef',
+    redirectUri: 'https://www.spotify.com/us/'
+});
 
 app.get('/', (req, res) => {
     track = spotifyApi.searchTracks('Love')
