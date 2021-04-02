07ac008cbac6cb6c7f37e7a618978a1e8205bef6
Tahmid Imran <tahmidimran1@gmail.com>
2021-04-02 17:55:42 -0500

    4 random songs based of genre

--- server/routes/spotifyRoutes.js
+++ server/routes/spotifyRoutes.js
@@ -6,18 +6,43 @@ module.exports = app => {
     clientSecret: keys.spotifyClientSecret
   });
   spotifyApi.setAccessToken(keys.spotifyAccessToken);
-  app.get("/spotify", (req, res) => {
-    res.send({ spotify: "there" });
-    track = spotifyApi.searchTracks("Love").then(
-      function(data) {
-        console.log('Search by "Love"', data.body);
-      },
-      function(err) {
-        console.error(err);
-      }
-    );
-    res.send(track);
+  app.get("/spotify/:genre", async (req, res) => {
+    try {
+      const data = await spotifyApi.searchTracks(`genre:${req.params.genre}`, {
+        limit: 4,
+        offset: Math.floor(Math.random() * 996)
+      });
+      const body = data.body.tracks.items;
+      let chosenCount = 0;
+      result = [];
+      body.map((track, index) => {
+        const album = {
+          name: track.album.name,
+          url: track.album.external_urls.spotify,
+          image: track.album.images[1].url
+        };
+        const artist = {
+          name: track.artists[0].name,
+          url: track.artists[0].external_urls.spotify
+        };
+        const song = {
+          name: track.name,
+          preview: track.preview_url
+        };
+        if (chosenCount == 1 || song.preview !== null) chosenCount++;
+        result.push({
+          song: song,
+          artist: artist,
+          album: album,
+          chosen: chosenCount === 1 ? true : false
+        });
+      });
+      res.status(200).send(result);
+    } catch (err) {
+      res.status(400).send({ error: err.body });
+    }
   });
+
   app.get("/spotify/access_token", (req, res) => {
     spotifyApi.clientCredentialsGrant().then(
       function(data) {
