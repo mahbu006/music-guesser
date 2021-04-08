eca584ed4dee960f2eff7202f7b224272789275c
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-04-08 16:13:43 -0500

    added mode, type, genre screens with wireflow

--- client/app-starter/App.js
+++ client/app-starter/App.js
@@ -1,15 +1,21 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
-import AuthenticationScreen from "./src/screens/Authentication";
+import AuthenticationScreen from "./src/screens/AuthenticationScreen";
+import ModeScreen from "./src/screens/ModeScreen";
+import GenreScreen from "./src/screens/GenreScreen";
+import TypeScreen from "./src/screens/TypeScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Authentication: AuthenticationScreen
+    Authentication: AuthenticationScreen,
+    Mode: ModeScreen,
+    Genre: GenreScreen,
+    Type: TypeScreen
   },
   {
-    initialRouteName: "Authentication",
+    initialRouteName: "Genre",
     defaultNavigationOptions: {
       title: "App",
     },
